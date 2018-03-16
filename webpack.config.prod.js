2ee478aea30e692dd36fedd4122b842c918c0ee5
Kaliyaperumal <mkaliyap@ceb.com>
2018-03-16 18:43:27 +0530

    Final Push

---
+++ webpack.config.prod.js
@@ -0,0 +1,62 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebPackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+export default {
+  debug: true,
+  devtool: 'source-map',
+  noInfo: false,
+  entry: {
+    vendor : path.resolve(__dirname, 'src/vendor'),
+    main : path.resolve(__dirname, 'src/index')
+  },
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].[chunkhash].js'
+  },
+  devserver: {
+    contentBase: path.resolve(__dirname, 'src')
+  },
+  plugins: [
+    //CSS
+    new ExtractTextPlugin('[name].[contenthash].css'),
+    //MD5 Hashing
+    new WebPackMd5Hash(),
+    //Bundle Splitting
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    //Create HTML file that includes reference to bundled JS
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true,
+      trackJSToken: '4975c79a913743de98adbfa0af947cd7'
+    }),
+    //Eliminate duplicate packages
+    new webpack.optimize.DedupePlugin(),
+    //Minify JS
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
+    ]
+  }
+}
